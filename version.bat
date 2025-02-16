afdf4860d4f2db31c13a875b4622297a2243263e
Kishore1407 <kishoresmart004@gmail.com>
2025-02-16 19:24:43 +0530

    Testing auto versioning

--- version.bat
+++ version.bat
@@ -13,9 +13,13 @@ set new_version=!major!.!minor!.!patch!
 
 echo !new_version! > version.txt
 
+
 git add version.txt
 git commit -m "Auto-incremented version to !new_version!"
+git tag -d v!new_version! 2>NUL
 git tag -a v!new_version! -m "Version !new_version!"
+
+git push origin v!new_version!
 
 echo New version set to !new_version!
 endlocal
